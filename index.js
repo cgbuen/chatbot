db6df9234e52286c83967ec9bd3047fd157cf1aa
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2019-06-04 19:52:30 -0700

    Fix bugs and refactor
    - Add proper content-type request header on initial auth request
    - Update refresh token when refreshing
    - Add more .catch()s and console.log()s
    - Refactor

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ const port = 3000
 app.get('/callback', (req, res) => {
   res.send('Authenticated.')
 
-  fetch('https://accounts.spotify.com/api/token', {
+  const spotifyOptionsAuth = {
     method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
     body: qs.stringify({
       grant_type: 'authorization_code',
       code: req.query.code,
@@ -20,52 +23,67 @@ app.get('/callback', (req, res) => {
       client_id: SPOTIFY_CLIENT_ID,
       client_secret: SPOTIFY_CLIENT_SECRET
     })
-  })
+  }
+
+  const twitchOptions = {
+    options: {
+      debug: true
+    },
+    connection: {
+      cluster: 'aws',
+      reconnect: true
+    },
+    identity: {
+      username: BOT_USER,
+      password: TWITCH_TOKEN
+    },
+    channels: [
+      CHANNEL
+    ]
+  }
+
+  fetch('https://accounts.spotify.com/api/token', spotifyOptionsAuth)
     .then(res => res.json())
     .then(tokens => {
       let accessToken = tokens.access_token
+      let refreshToken = tokens.refresh_token
 
-      const options = {
-        options: {
-          debug: true
-        },
-        connection: {
-          cluster: 'aws',
-          reconnect: true
-        },
-        identity: {
-          username: BOT_USER,
-          password: TWITCH_TOKEN
-        },
-        channels: [
-          CHANNEL
-        ]
-      }
-
-      const client = new twitch.client(options)
-
+      const client = new twitch.client(twitchOptions)
       client.connect()
 
       const handleMessaging = cp => {
         if (cp.error && cp.error.includes('xpire')) {
+          console.log('==> expiration error:', cp.error)
           fetch('https://accounts.spotify.com/api/token', {
             method: 'POST',
             body: qs.stringify({
               grant_type: 'refresh_token',
-              refresh_token: tokens.refresh_token
+              refresh_token: refreshToken
             })
           })
             .then(res => res.json())
-            .then(subTokens => {
-              accessToken = subTokens.access_token
+            .then(updatedTokens => {
+              accessToken = updatedTokens.access_token
+              refreshToken = updatedTokens.refresh_token
               fetch('https://api.spotify.com/v1/me/player/currently-playing', {
                 headers: {
                   Authorization: `Bearer ${accessToken}`
                 }
               })
-                .then(handleMessaging)
+                .catch(err => {
+                  const msg = 'chatbot/spotify integration is broken lmao'
+                  console.log('==> caught error trying to perform refresh request', err)
+                  client.action(CHANNEL, msg)
+                })
+            })
+            .then(handleMessaging)
+            .catch(err => {
+              const msg = 'chatbot/spotify integration is broken lmao'
+              console.log('==> caught error in refresh procedure', err)
+              client.action(CHANNEL, msg)
             })
         } else if (cp.error) {
+          console.log('==> miscellaneous error:', cp.error)
           throw new Error(cp.error)
         }
         if (cp.is_playing && cp.item) {
@@ -73,12 +91,10 @@ app.get('/callback', (req, res) => {
           const title = cp.item.name || 'n/a'
           const album = (cp.item.album && cp.item.album.name) || 'n/a'
           const msg = `${artists} - ${title} [${album}]`
-          console.log(`==> response to ${user}: ${msg}`)
           client.action(CHANNEL, msg)
         } else {
           const msg = 'spotify\'s not playing anything rn'
-          console.log('cp', cp)
-          console.log(`==> response to ${user}: ${msg}`)
+          console.log('==> spotify not playing anything; cp', cp)
           client.action(CHANNEL, msg)
         }
       }
@@ -97,17 +113,22 @@ app.get('/callback', (req, res) => {
             .then(handleMessaging)
             .catch(err => {
               const msg = 'chatbot/spotify integration is broken lmao'
-              console.log(`==> response to ${user}: ${msg}`)
+              console.log('==> caught error in chat action', err)
               client.action(CHANNEL, msg)
             })
           
         }
       })
     })
+    .catch(err => {
+      const msg = 'chatbot/spotify integration is broken lmao'
+      console.log('==> caught error trying to perform initial auth', err)
+      client.action(CHANNEL, msg)
+    })
 
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Spotify callback app listening on port ${port}`))
 
 const query = {
   client_id: SPOTIFY_CLIENT_ID,
