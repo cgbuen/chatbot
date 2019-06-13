575adaa8d1de5aa0750bd8bfa50aafb7319b20c0
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2019-06-13 02:52:59 -0700

    Upgrade to twitch-js 2 beta
    - Add !so command with user-checking via `api` in twitch-js 2
    
    Somewhat unrelated:
    - Reorganize message text into `MSGS` object (per Alvaro review)
    - Add !devices command, updating spotify scope to properly get data
    
    TODO:
    - Restrict !so to followers
    - Restrict !devices to me, put MSGS in its own file

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const express = require('express')
-const csrf = require('csrf')
-const twitch = require('twitch-js')
+const Csrf = require('csrf')
+const TwitchJs = require('twitch-js').default
 const qs = require('qs')
 const open = require('open')
 const requestSpotify = require('./request-spotify')
 const { BOT_USER, CHANNEL, GAME_ID, TWITCH_TOKEN, SPOTIFY_CLIENT_ID } = require('./vars')
 
-const csrfGenerator = new csrf()
+const csrfGenerator = new Csrf()
 const CSRF_SECRET = csrfGenerator.secretSync()
 const CSRF_TOKEN = csrfGenerator.create(CSRF_SECRET)
-const MSG_BROKEN = 'chatbot/spotify integration is broken lmao'
-const MSG_NOT_PLAYING = 'i\'m not playing anything on spotify rn'
 const COUNT_RETRIES = 3
+const MSGS = {
+  BROKEN_SPOTIFY: 'chatbot/spotify integration is broken lmao',
+  BROKEN_SHOUT: 'chatbot shoutouts are broken lol',
+  NOT_PLAYING: 'i\'m not playing anything on spotify rn',
+  INVALID_USER: 'bro i don\'t think this guy is real'
+}
 const TWITCH_OPTIONS = {
-  options: { debug: true },
-  connection: { cluster: 'aws', reconnect: true },
-  identity: { username: BOT_USER, password: TWITCH_TOKEN },
-  channels: [ CHANNEL ]
+  token: TWITCH_TOKEN,
+  username: BOT_USER
 }
 
 const app = express()
@@ -37,8 +39,9 @@ app.get('/callback', async (req, res) => {
   let accessToken = spotifyTokenData.access_token
   let refreshToken = spotifyTokenData.refresh_token
 
-  const client = new twitch.client(TWITCH_OPTIONS)
-  client.connect() // only connect to twitch if spotify auth was successful
+  const { chat, api } = new TwitchJs(TWITCH_OPTIONS)
+  await chat.connect() // only connect to twitch if spotify auth was successful
+  await chat.join(CHANNEL)
 
   const handleMessaging = async (currentlyPlayingData, options) => {
     // handle messaging based off of the currentlyPlayingData provided by
@@ -48,7 +51,7 @@ app.get('/callback', async (req, res) => {
       // if too many refresh attempts or attempts not specified, just tell the
       // user it's broken
       console.log('==> Too many refresh attempts.')
-      return client.action(CHANNEL, MSG_BROKEN)
+      return chat.say(CHANNEL, MSGS.BROKEN_SPOTIFY)
 
     } else if (currentlyPlayingData && currentlyPlayingData.error && currentlyPlayingData.error.message && currentlyPlayingData.error.message.includes('xpire')) {
       // if expired error, retry using refresh token. recursively call,
@@ -65,37 +68,57 @@ app.get('/callback', async (req, res) => {
     } else if (currentlyPlayingData && currentlyPlayingData.error) {
       // if other error, just tell the user it's broken
       console.log(`==> Miscellaneous error: ${currentlyPlayingData.error}`)
-      return client.action(CHANNEL, MSG_BROKEN)
+      return chat.say(CHANNEL, MSGS.BROKEN_SPOTIFY)
 
-    } else if (currentlyPlayingData && currentlyPlayingData.is_playing && currentlyPlayingData.item) {
+    } else if (currentlyPlayingData && currentlyPlayingData.is_playing && currentlyPlayingData.item && currentlyPlayingData.device) {
       // if you are playing something, display
       console.log('==> Song currently playing.')
       const artists = (currentlyPlayingData.item.artists && currentlyPlayingData.item.artists.map(item => item.name).join(', ')) || 'n/a'
       const title = currentlyPlayingData.item.name || 'n/a'
       const album = (currentlyPlayingData.item.album && currentlyPlayingData.item.album.name) || 'n/a'
+      // const device = currentlyPlayingData.device.name
       const msg = `${artists} - ${title} [${album}]`
-      return client.action(CHANNEL, msg)
+      return chat.say(CHANNEL, msg)
 
     } else if (currentlyPlayingData) {
       // if not playing anything, tell user you're not playing anything
       console.log(`==> No song currently playing.${currentlyPlayingData.noSession ? ' (No current session.)' : ''}`)
-      return client.action(CHANNEL, MSG_NOT_PLAYING)
+      return chat.say(CHANNEL, MSGS.NOT_PLAYING)
 
     } else {
       // fallback, e.g. if data wasn't ever even put into function, tell user
       // it's broken
       console.log('==> currentlyPlayingData not provided.')
-      return client.action(CHANNEL, MSG_BROKEN)
+      return chat.say(CHANNEL, MSGS.BROKEN_SPOTIFY)
     }
   }
 
-  client.on('chat', async (channel, user, message, self) => {
+  chat.on('PRIVMSG/#cgbuen', async ({ message }) => {
     if (message === '!fc') {
-      return client.action(CHANNEL, GAME_ID)
+      return chat.say(CHANNEL, GAME_ID)
     }
     if (message === '!song') {
       const spotifyCurrentlyPlayingData = await requestSpotify.currentlyPlaying(accessToken)
       return handleMessaging(spotifyCurrentlyPlayingData, { retries: COUNT_RETRIES })
+    }
+    if (/^\!so\s[\w]+$/.test(message)) {
+      const userInput = message.match(/^\!so\s([\w]+)$/)[1]
+      try {
+        const userObj = await api.get('users', { search: { login: userInput } })
+        if (userObj.total === 1) {
+          return chat.say(CHANNEL, `shouts out ${userInput}`)
+        } else {
+          return chat.say(CHANNEL, MSGS.INVALID_USER)
+        }
+      } catch (e) {
+        console.log('==> Request fetch error api user', e)
+        return chat.say(CHANNEL, MSGS.BROKEN_SHOUT)
+      }
+    }
+    if (message === '!devices') {
+      const spotifyDeviceData = await requestSpotify.devices(accessToken)
+      const deviceMsg = spotifyDeviceData.devices.map(device => `${device.name} (${device.type})`).join(', ') || 'no devices'
+      return chat.say(CHANNEL, deviceMsg)
     }
   })
 })
@@ -107,7 +130,7 @@ const query = {
   response_type: 'code',
   redirect_uri: 'http://localhost:3000/callback',
   state: CSRF_TOKEN,
-  scope: 'user-read-currently-playing',
+  scope: 'user-read-playback-state',
 }
 const url = `https://accounts.spotify.com/authorize?${qs.stringify(query)}`
 console.log(`==> Opening url: ${url}.`)
