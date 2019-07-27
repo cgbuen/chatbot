468b36b665bc1732a0a7b77638916486ef08c895
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2019-07-26 23:49:27 -0700

    Add !charity alias for !donate / !support

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ app.get('/callback', async (req, res) => {
             return chat.say(CHANNEL, MSGS.BROKEN_SHOUT)
           }
         }
-        if (['!support', '!donate', '!bits', '!sub', '!subs', '!subscribe'].includes(message)) {
+        if (['!charity', '!support', '!donate', '!bits', '!sub', '!subs', '!subscribe'].includes(message)) {
           const msg = 'All proceeds go to the San Francisco-Marin Food Bank. You can donate directly here: https://us-p2p.netdonor.net/1803/general/61375/cgbuen'
           botLog(msg)
           return chat.say(CHANNEL, msg)
