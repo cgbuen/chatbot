15d93f50489771fc105b089d3fe9a8219a5c111f
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2020-02-12 00:44:04 -0800

    Add better token logic and express endpoints
    - Dedicate directory to storing tokens, while removing use of
      TWITCH_TOKEN
    - Update README
    - Fix so.js to use api passed in from chat endpoint in order to find out
      if a user is real
    - Fix spotify-devices to properly use request-spotify and get token from
      store
    - Simplify spotify-song so that it only translates response JSON into
      proper messages, moving all of the refresh/retry logic into
      request-spotify
    - Remove unnecessary variables passed in to spotify-devices and
      spotify-song
    - Remove re-getting undefined refresh token for spotify which doesn't
      seem to spit out new refresh tokens after refreshing (anymore?)
    - Update -callback endpoint
    - Add a request-twitch file (similar to request-spotify)
    - Add endpoints to initialize tokens into stores
    - Add /chat endpoint specifically for kicking off chatbot connection,
      redirecting to twitch dashboard

---
+++ helpers/request-twitch.js
@@ -0,0 +1,100 @@
+const fs = require('fs')
+const fetch = require('node-fetch')
+const TwitchJs = require('twitch-js').default
+const qs = require('qs')
+const { BOT_USER, TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET } = require('../vars')
+
+const auth = async (authCode) => {
+  // initial authorization
+  const twitchOptionsAuth = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    body: qs.stringify({
+      grant_type: 'authorization_code',
+      code: authCode,
+      redirect_uri: 'http://localhost:3000/twitch-callback',
+      client_id: TWITCH_CLIENT_ID,
+      client_secret: TWITCH_CLIENT_SECRET
+    })
+  }
+  let data
+  try {
+    const twitchResponseAuth = await fetch('https://id.twitch.tv/oauth2/token', twitchOptionsAuth)
+    data = await twitchResponseAuth.json()
+  } catch (e) {
+    console.log('==> Request fetch error auth', e)
+    data = {
+      error: 'fetch error: auth'
+    }
+  }
+  return data
+}
+
+const refresh = async (refreshToken) => {
+  // refresh token for new access tokens
+  const twitchOptionsRefresh = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    body: qs.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken,
+      client_id: TWITCH_CLIENT_ID,
+      client_secret: TWITCH_CLIENT_SECRET
+    })
+  }
+  let data
+  try {
+    const twitchResponseRefresh = await fetch('https://id.twitch.tv/oauth2/token', twitchOptionsRefresh)
+    data = await twitchResponseRefresh.json()
+    fs.writeFileSync('./token-store/twitch-access', data.access_token)
+    fs.writeFileSync('./token-store/twitch-refresh', data.refresh_token)
+  } catch(e) {
+    console.log('==> Request fetch error refresh', e)
+    data = {
+      error: 'fetch error: refresh'
+    }
+  }
+  return data
+}
+
+const getAllStats = async (accessToken, { retries = 3 } = {}) => {
+  let data
+  if (!retries) {
+    console.log('** too many twitch stats refresh attempts')
+    data = { error: 'Too many Statsrefresh attempts.', }
+    return data
+  }
+  try {
+    const { api } = new TwitchJs({
+      token: accessToken,
+      username: BOT_USER
+    })
+    const users = await api.get('users', { version: 'helix', search: { login: BOT_USER } })
+    const subs = await api.get('subscriptions', { version: 'helix', search: { broadcaster_id: users.data[0].id } })
+    data = {
+      subs
+    }
+  } catch (e) {
+    console.log('==> Request twitch api fetch error', e)
+    if (e.body.error === 'Unauthorized') {
+      console.log('** Unauthorized getAllStats response data')
+      const twitchTokenDataUpdated = await refresh(fs.readFileSync('./token-store/twitch-refresh', 'utf8'))
+      data = await getAllStats(twitchTokenDataUpdated.access_token, { retries: retries - 1 }) // try again after tokens updated
+    } else {
+      data = {
+        error: 'twitch api fetch error: stats'
+      }
+    }
+  }
+  return data
+}
+
+module.exports = {
+  auth,
+  refresh,
+  getAllStats
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const fetch = require('node-fetch')
 const moment = require('moment')
 const express = require('express')
 const Csrf = require('csrf')
@@ -6,15 +7,15 @@ const TwitchJs = require('twitch-js').default
 const qs = require('qs')
 const open = require('open')
 const bitRegExp = require('./helpers/bitRegExp')
-const requestSpotify = require('./request-spotify')
-const requestNintendo = require('./request-nintendo')
-const { BOT_USER, CHANNEL, TWITCH_TOKEN, SPOTIFY_CLIENT_ID, COUNTER } = require('./vars')
+const requestSpotify = require('./helpers/request-spotify')
+const requestTwitch = require('./helpers/request-twitch')
+const { BOT_USER, CHANNEL, SPOTIFY_CLIENT_ID, TWITCH_CLIENT_ID, COUNTER } = require('./vars')
 
 const csrfGenerator = new Csrf()
 const CSRF_SECRET = csrfGenerator.secretSync()
 const CSRF_TOKEN = csrfGenerator.create(CSRF_SECRET)
 const TWITCH_OPTIONS = {
-  token: TWITCH_TOKEN,
+  token: fs.readFileSync('./token-store/twitch-access', 'utf8').trim(),
   username: BOT_USER
 }
 
@@ -34,20 +35,85 @@ if (!fs.existsSync(`./${COUNTER}.json`)) {
   fs.writeFileSync(`./${COUNTER}.json`, '{}')
 }
 
-app.get('/callback', async (req, res) => {
+app.get('/initialize-spotify-token', async (req, res) => {
+  const query = {
+    client_id: SPOTIFY_CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: 'http://localhost:3000/spotify-callback',
+    state: CSRF_TOKEN,
+    scope: 'user-read-playback-state',
+  }
+  const url = `https://accounts.spotify.com/authorize?${qs.stringify(query)}`
+  return res.redirect(url)
+})
+
+app.get('/spotify-callback', async (req, res) => {
   if (!csrfGenerator.verify(CSRF_SECRET, req.query.state)) {
-    // spotify recommends the use of the state parameter for CSRF protection.
-    // as a separate note, spotify additionally has extra redirection
-    // protection through the use of a configurable whitelist from your app's
-    // dashboard.
     return res.send('Authentication failed.')
   }
+  const tokenData = await requestSpotify.auth(req.query.code)
+  fs.writeFileSync('./token-store/spotify-access', tokenData.access_token)
+  fs.writeFileSync('./token-store/spotify-refresh', tokenData.refresh_token)
+  return res.send('Stored Spotify tokens.')
+})
 
-  res.send('Authentication successful. You can now close this page.')
+app.get('/initialize-twitch-token', async (req, res) => {
+  const query = {
+    client_id: TWITCH_CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: 'http://localhost:3000/twitch-callback',
+    state: CSRF_TOKEN,
+    force_verify: true,
+    scope: [
+      'channel:read:subscriptions',
+      'bits:read',
+      'chat:read',
+      'chat:edit',
+      'channel:moderate',
+      'whispers:read',
+      'whispers:edit',
+      'channel_editor',
+      'channel_subscriptions',
+    ].join('__PLUS__')
+  }
+  const url = `https://id.twitch.tv/oauth2/authorize?${qs.stringify(query)}`.replace(/__PLUS__/g, '+')
+  return res.redirect(url)
+})
 
-  const spotifyTokenData = await requestSpotify.auth(req.query.code)
+app.get('/twitch-callback', async (req, res) => {
+  if (!csrfGenerator.verify(CSRF_SECRET, req.query.state)) {
+    return res.send('Authentication failed.')
+  }
+  const tokenData = await requestTwitch.auth(req.query.code)
+  fs.writeFileSync('./token-store/twitch-access', tokenData.access_token)
+  fs.writeFileSync('./token-store/twitch-refresh', tokenData.refresh_token)
+  return res.send('Stored Twitch tokens.')
+})
+
+app.get('/spotify.json', async (req, res) => {
+  const playing = await requestSpotify.currentlyPlaying(fs.readFileSync('./token-store/spotify-access', 'utf8').trim())
+  return res.send(playing)
+})
+
+app.get('/twitch.json', async (req, res) => {
+  const subs = await requestTwitch.getAllStats(fs.readFileSync('./token-store/twitch-access', 'utf8').trim())
+  return res.send(subs)
+})
+
+app.get('/stats.json', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  return res.send(fs.readFileSync(`${counterDir}/${COUNTER}.json`))
+})
+
+app.get('/player.json', async (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  const requestNintendo = require('./request-nintendo')
+  return res.send(await requestNintendo.requestPlayer())
+})
+
+app.get('/chat', async (req, res) => {
   const { chat, api } = new TwitchJs(TWITCH_OPTIONS)
-  await chat.connect() // only connect to twitch if spotify auth was successful
+  await chat.connect()
   await chat.join(CHANNEL)
 
   chat.on('*', async (msg) => {
@@ -72,13 +138,13 @@ app.get('/callback', async (req, res) => {
           msg = require('./commands/controls')()
         }
         if (message === '!song') {
-          msg = await require('./commands/spotify-song')({ spotifyTokenData })
+          msg = await require('./commands/spotify-song')()
         }
         if (message === '!devices') {
-          msg = await require('./commands/spotify-devices')({ spotifyTokenData })
+          msg = await require('./commands/spotify-devices')()
         }
         if (/^\!(so|shoutout)\s@?[\w]+(\s|$)/.test(message)) {
-          msg = require('./commands/so')({ message })
+          msg = await require('./commands/so')({ message, api })
         }
         if (['!charity', '!support', '!donate', '!bits', '!sub', '!subs', '!subscribe'].includes(message)) {
           msg = require('./commands/charity')()
@@ -106,27 +172,8 @@ app.get('/callback', async (req, res) => {
       }
     }
   })
-})
-
-app.get('/stats.json', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  return res.send(fs.readFileSync(`${counterDir}/${COUNTER}.json`))
-})
-
-app.get('/player.json', async (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  return res.send(await requestNintendo.requestPlayer())
+  res.redirect('https://dashboard.twitch.tv/u/cgbuen/stream-manager')
 })
 
 app.listen(port, () => console.log(`Spotify callback API endpoint app listening on port ${port}.`))
-
-const query = {
-  client_id: SPOTIFY_CLIENT_ID,
-  response_type: 'code',
-  redirect_uri: 'http://localhost:3000/callback',
-  state: CSRF_TOKEN,
-  scope: 'user-read-playback-state',
-}
-const url = `https://accounts.spotify.com/authorize?${qs.stringify(query)}`
-console.log(`==> Opening url: ${url}.`)
-open(url)
+open('http://localhost:3000/chat')
