d830069bb51bd580af5102b3b4ffde13bb47291e
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2020-09-11 16:48:46 -0700

    Add build command

---
+++ commands/build.js
@@ -0,0 +1,4 @@
+module.exports = function build() {
+  const msg = 'mobo: x570 tomahawk, cpu: ryzen 9 3900xt, cooler: noctua nh-d15, memory: gskill trident z neo 64gb 3600 CL16, storage: sx8200 2TB NVME, sabrent rocket 4.0 500GB nvme, case: fractal design define r6 USB-C blackout, psu: fractal design ion+ 860W, gpu: evga gtx 1660 super'
+  return msg
+}
